5d33eb2e2990db4a96b5ed7f19d196f5a5700cd8
Fernando Emir Garade <fernandoemirgarade@MacBook-Pro-de-Fernando.local>
2023-01-30 22:58:25 -0300

    First commit of files

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This repository was created when doing the 365 Careers Credit Risk Modelling Cou
 Link to the course: https://www.udemy.com/course/credit-risk-modeling-in-python/
 
 You can find here the notebooks developed during the course and an extra last notebook where a new data input is processed through the models and the final expected loss prediction is built.
+
+Datasets are not uploaded here for 2 reasons: it's size and 365 Careers' ownership.
---
+++ custom_model_classes.py
@@ -0,0 +1,41 @@
+# P values for sklearn logistic regression.
+
+# Class to display p-values for logistic regression in sklearn.
+from sklearn import linear_model
+
+class LogisticRegression_with_p_values:
+    
+    def __init__(self,*args,**kwargs):#,**kwargs):
+        self.model = linear_model.LogisticRegression(*args,**kwargs)#,**args)
+
+    def fit(self,X,y):
+        self.model.fit(X,y)
+        
+        #### Get p-values for the fitted model ####
+        denom = (2.0 * (1.0 + np.cosh(self.model.decision_function(X))))
+        denom = np.tile(denom,(X.shape[1],1)).T
+        F_ij = np.dot((X / denom).T,X) ## Fisher Information Matrix
+        Cramer_Rao = np.linalg.inv(F_ij) ## Inverse Information Matrix
+        sigma_estimates = np.sqrt(np.diagonal(Cramer_Rao))
+        z_scores = self.model.coef_[0] / sigma_estimates # z-score for eaach model coefficient
+        p_values = [stat.norm.sf(abs(x)) * 2 for x in z_scores] ### two tailed test for p-values
+        
+        self.coef_ = self.model.coef_
+        self.intercept_ = self.model.intercept_
+        self.p_values = p_values
+        
+class LinearRegression(linear_model.LinearRegression):
+    def __init__(self, fit_intercept=True, normalize=False, copy_X=True,
+                 n_jobs=1):
+        super().__init__(fit_intercept=fit_intercept, normalize=normalize, copy_X=copy_X, n_jobs=n_jobs)
+        self.fit_intercept = fit_intercept
+        self.normalize = normalize
+        self.copy_X = copy_X
+        self.n_jobs = n_jobs
+    def fit(self, X, y, n_jobs=1):
+        self = super(LinearRegression, self).fit(X, y, n_jobs)
+        sse = np.sum((self.predict(X) - y) ** 2, axis=0) / float(X.shape[0] - X.shape[1])
+        se = np.array([np.sqrt(np.diagonal(sse * np.linalg.inv(np.dot(X.T, X))))])
+        self.t = self.coef_ / se
+        self.p = np.squeeze(2 * (1 - stat.t.cdf(np.abs(self.t), y.shape[0] - X.shape[1])))
+        return self
